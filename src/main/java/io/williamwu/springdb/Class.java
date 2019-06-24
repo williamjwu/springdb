1dfaeecc410b5cec0c9486193c68f72940ab0096
William Wu <williamwu2049@outlook.com>
2019-06-24 18:42:24 +0800

    finish student and 'class' class

--- src/main/java/io/williamwu/springdb/Class.java
+++ src/main/java/io/williamwu/springdb/Class.java
@@ -1,16 +1,43 @@
 package io.williamwu.springdb;
 
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Class {
     final private String id;
-    private String name;
+    final private String className;
+    // there is only one class
     private List<String> students;
-    private List<Enums.Days> classDays;
-    private List<Enums.Periods> classPeriods;
+    // days and periods are 1 to 1 on same digits
+    // ex: classDays[0] = WEDNESDAY, classPeriods[0] = SECOND
+    // this means there is a class on Wednesday second period
+    // a class can have multiple meeting periods
+    private Enums.Days[] classDays;
+    private Enums.Periods[] classPeriods;
 
-    public Class(String name) {
-        this.name = name;
-        id = Functions.toSHA256(name);
+    public Class(String className, Enums.Days[] classDays, Enums.Periods[] classPeriods) {
+        this.className = className;
+        this.classDays = classDays;
+        this.classPeriods = classPeriods;
+        students = new LinkedList<>();
+        id = Functions.toSHA256(className);
+    }
+
+    // accessors and modifiers
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return className;
+    }
+    public Iterator<String> getStudents() {
+        return students.iterator();
+    }
+    public void addStudent(String currStudent) {
+        students.add(currStudent);
+    }
+    public boolean rmStudent(String currStudent) {
+        return students.remove(currStudent);
     }
 }
--- src/main/java/io/williamwu/springdb/Student.java
+++ src/main/java/io/williamwu/springdb/Student.java
@@ -6,28 +6,28 @@ import java.util.List;
 
 public class Student {
     final private String id;
-    private String name;
+    private String studentName;
     private int age;
     private List<String> classes;
     private Enums.Gender gender;
 
-    public Student(String name, int age, Enums.Gender gender) {
-        this.name = name;
+    public Student(String studentName, int age, Enums.Gender gender) {
+        this.studentName = studentName;
         this.age = age;
         this.gender = gender;
         classes = new LinkedList<>();
-        id = Functions.toSHA256(name);
+        id = Functions.toSHA256(studentName);
     }
 
     // accessors and modifiers
     public String getId() {
         return id;
     }
-    public String getName() {
-        return name;
+    public String getStudentName() {
+        return studentName;
     }
-    public void setName(String name) {
-        this.name = name;
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
     }
     public int getAge() {
         return age;
@@ -40,14 +40,14 @@ public class Student {
      * stores all class id instead of class objects
      * all class objects can be accessed in main method's hashmap
      */
-    public Iterator<String> getAllClasses() {
+    public Iterator<String> getClasses() {
         return classes.iterator();
     }
-    public void addClasses(String currClass) {
+    public void addClass(String currClass) {
         classes.add(currClass);
     }
     // returns true if the class presents, used for detecting a valid class is removed
-    public boolean rmClasses(String currClass) {
+    public boolean rmClass(String currClass) {
         return classes.remove(currClass);
     }
 
