225d0699413db341d1db373728d042ff6072f6b0
William Wu <williamwu2049@outlook.com>
2019-07-03 13:12:41 +0800

    update pom.xmlh

--- src/main/java/io/williamwu/springdb/SpringdbApplication.java
+++ src/main/java/io/williamwu/springdb/SpringdbApplication.java
@@ -3,8 +3,10 @@ package io.williamwu.springdb;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 @MapperScan(basePackages = "io.williamwu.springdb.mapper")
 public class SpringdbApplication {
     public static void main(String[] args) {
