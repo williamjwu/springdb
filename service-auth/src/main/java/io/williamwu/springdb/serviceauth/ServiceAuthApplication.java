94cb5636a15a3b5b41af509aafefbb13481f80ea
WuJiajie <williamwu2049@outlook.com>
2019-07-12 18:38:58 +0800

    update

--- service-auth/src/main/java/io/williamwu/springdb/serviceauth/ServiceAuthApplication.java
+++ service-auth/src/main/java/io/williamwu/springdb/serviceauth/ServiceAuthApplication.java
@@ -2,8 +2,12 @@ package io.williamwu.springdb.serviceauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableAuthorizationServer
 public class ServiceAuthApplication {
 
     public static void main(String[] args) {
