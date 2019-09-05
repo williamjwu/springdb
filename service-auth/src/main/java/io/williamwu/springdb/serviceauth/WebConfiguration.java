0a9a50bf52dd3d444062f443119e9963acad6bbb
William Wu <williamwu2049@outlook.com>
2019-09-05 15:08:56 -0500

    split zuul into authentication server and api gateway server

---
+++ service-auth/src/main/java/io/williamwu/springdb/serviceauth/WebConfiguration.java
@@ -0,0 +1,25 @@
+package io.williamwu.springdb.serviceauth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+        http
+                .csrf()
+                .disable();
+    }
+}
---
+++ service-gateway/src/main/java/io/williamwu/springdb/servicegateway/ServiceGatewayApplication.java
@@ -0,0 +1,19 @@
+package io.williamwu.springdb.servicegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ServiceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGatewayApplication.class, args);
+    }
+
+}
