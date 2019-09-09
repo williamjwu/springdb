2537d674428e9365570439f72c2f96bd7453a9ef
William Wu <williamwu2049@outlook.com>
2019-09-09 15:31:08 -0500

    update maven compile java language level to 11

---
+++ service-auth/src/main/java/io/williamwu/springdb/serviceauth/OAuthLoginController.java
@@ -0,0 +1,8 @@
+package io.williamwu.springdb.serviceauth;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OAuthLoginController {
+
+}
--- service-auth/src/main/java/io/williamwu/springdb/serviceauth/ServiceAuthApplication.java
+++ service-auth/src/main/java/io/williamwu/springdb/serviceauth/ServiceAuthApplication.java
@@ -2,9 +2,11 @@ package io.williamwu.springdb.serviceauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 @EnableEurekaClient
 public class ServiceAuthApplication {
 
